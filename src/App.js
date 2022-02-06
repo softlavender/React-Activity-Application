20fe21cc998037a8a5a3ff7148ffe8f1bc9983c8
halmur <halidmuratovic7@gmail.com>
2022-02-06 17:52:21 +0100

    Minor changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Activity from "./components/Activity";
 function App() {
   const [myActivity, setMyActivity] = useState()
 
-  const getActivity = async (wantedActivityAPI, e) => {
-    const newActivityData = await fetch(e.target.value ? `${wantedActivityAPI + '?participants=' + e.target.value}` : wantedActivityAPI)
+  const getActivity = async (wantedEndpoint, e) => {
+    const endpointVal = Array.from(e.target.children[0].children[1].childNodes).find(x => x.selected)
+    
+    const newActivityData = await fetch(endpointVal.value > 0 ? `${wantedEndpoint + '?participants=' + endpointVal.value}` : wantedEndpoint)
     const newActivity = await newActivityData.json()
     setMyActivity(newActivity)
   }
@@ -20,31 +22,26 @@ function App() {
       
       <main className="App-main">
         <section id='activities'>
-          {myActivity ? myActivity && <Activity {...myActivity}/> : <h1>Click the "Find Activity" button for an random activity to do :)</h1>}
+          {myActivity ? myActivity && <Activity {...myActivity}/> : <h1>Click the <span>"Find Activity"</span> button for an random activity :)</h1>}
         </section>
         
 
-        <div className='activity-controls'>
-          <label htmlFor='random-activity'>Find an <span>Activity</span> with any amount of participants</label>
-          <button id="random-activity" onClick={ e => getActivity('http://www.boredapi.com/api/activity/', e)}>
-            find activity
-          </button>
-        </div>
-
-        <div className='activity-controls specify-activity-participants'>
-          <form onChange={e => e.preventDefault(getActivity('http://www.boredapi.com/api/activity', e))}>
-            <label htmlFor='participants'>Find an <span>Activity</span> with 1, 2 or 4 participants</label>
+        <form id='activity-controls' onSubmit={e => e.preventDefault(getActivity('http://www.boredapi.com/api/activity', e))}>
+          <div id='select-participants'>
+            <label htmlFor='participants'>Find an Activity with 1, 2 or 4 participants <span>(Optional)</span></label>
             <select name="participants" id='participants'>
-              {/* selected attributet ger ett varning i konsolen som jag inte kan lösa:
-                    * Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>.
-              */}
-              <option selected disabled hidden>select participants</option>
-              <option value='1'>one</option>
-              <option value='2'>two</option>
-              <option value='4'>four</option>
+              <option value='random'>Random participants</option>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='4'>4</option>
             </select>
-          </form>
-        </div>
+          </div>
+          
+          <div id='select-random-activity'>
+            <label htmlFor='random-activity'></label>
+            <button id="random-activity">find activity</button>
+          </div>
+        </form>
       </main>
     </div>
   );
